2f1d70c36f6816fd868502ee6aa5be17aad5b7e9
CorteFacil <146096163+CorteFacil@users.noreply.github.com>
2026-06-26 15:13:48 -0300

    Update HospedeService.js

--- src/services/HospedeService.js
+++ src/services/HospedeService.js
@@ -1,4 +1,5 @@
-import {Hospede} from '../models/Hospede.js';
+import { Hospede } from '../models/Hospede.js';
+import { Reserva } from '../models/Reserva.js';
 
 class HospedeService {
     static async findAll() {
@@ -20,7 +21,12 @@ class HospedeService {
 
     static async create(req, res) {     
         const { nome, cpfPassaporte, email, telefone, nascimento, estado } = req.body;
+        
         if (estado == null) throw 'Estado deve ser preenchido!';
+
+        const hospedeExistente = await Hospede.findOne({ where: { cpfPassaporte } });
+        if (hospedeExistente) throw 'Já existe um hóspede cadastrado com este Documento (CPF ou Passaporte)!';
+
         const obj = await Hospede.create({ nome, cpfPassaporte, email, telefone, nascimento, estadoId: estado.id});
         return await Hospede.findByPk(obj.id, {include: { all:true, nested: true}});
     }
@@ -28,26 +34,41 @@ class HospedeService {
     static async update(req, res) {
         const { id } = req.params;
         const { nome, cpfPassaporte, email, telefone, nascimento, estado } = req.body;
+        
         if (estado == null) throw 'Estado deve ser preenchido!';
+        
         const obj = await Hospede.findByPk(id, {include: {all: true, nested: true } });
         if (obj == null) throw 'Hóspede não encontrado!';
+
+        const hospedeExistente = await Hospede.findOne({ where: { cpfPassaporte } });
+        if (hospedeExistente && hospedeExistente.id != id) {
+            throw 'Este Documento (CPF ou Passaporte) já está sendo utilizado por outro hóspede no sistema!';
+        }
+
         Object.assign(obj, {nome, cpfPassaporte, email, telefone, nascimento, estadoId: estado.id});
-         await obj.save();
+        await obj.save();
+        
         return await Hospede.findByPk(id, {include: {all: true, nested: true } });
     }       
 
     static async delete(req, res) { 
         const { id } = req.params; 
         const obj = await Hospede.findByPk(id);
+        
         if (obj == null) throw 'Hóspede não encontrado!';
+        
+        const totalReservas = await Reserva.count({ where: { hospedeId: id } });
+        if (totalReservas > 0) {
+            throw "Não é possível remover um hóspede que possui reservas cadastradas.";
+        }
+
         try {
             await obj.destroy();
             return obj;
         } catch(erro) {
-            throw "Não e possível remover um hóspede que possui reservas";
+            throw "Erro ao tentar remover o hóspede do banco de dados.";
         }
     }
-
 }
 
 export { HospedeService };
